d8e39df683e089d1b60847813ba19d008376f03b
iourilitv <iourilitv1965@gmail.com>
2020-11-28 17:18:01 +0300

    Completed whole lesson6 hw.

---
+++ lesson6-app/src/main/java/gb/lesson6/controllers/CartController.java
@@ -0,0 +1,48 @@
+package gb.lesson6.controllers;
+
+import gb.lesson6.reprentities.OrderItemRepr;
+import gb.lesson6.services.CartService;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class CartController implements Serializable {
+
+    @EJB
+    private CartService cartService;
+
+    private List<OrderItemRepr> items;
+
+    private BigDecimal total;
+
+    public void preloadData(ComponentSystemEvent componentSystemEvent) {
+        items = cartService.getItems();
+        total = cartService.getTotal();
+    }
+
+    public List<OrderItemRepr> getItems() {
+        return items;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public String addItem(OrderItemRepr itemRepr) {
+        cartService.addItem(itemRepr);
+        return "/cart.xhtml?faces-redirect=true";
+    }
+
+    public String deleteItem(OrderItemRepr itemRepr) {
+        cartService.deleteItem(itemRepr);
+        return "/cart.xhtml?faces-redirect=true";
+    }
+
+}
